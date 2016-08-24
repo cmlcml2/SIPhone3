41282e13f150fa6a5974392e5b1a25624514dfed
cmlcml2 <cyril.lou94@hotmail.fr>
2016-08-24 15:21:13 +0200

    Swipe Sans Slide Debug

--- app/pages/detailphone/detailphone.ts
+++ app/pages/detailphone/detailphone.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams,MenuController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 
 /*
   Generated class for the DetailphonePage page.
-
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
@@ -16,33 +15,22 @@ export class DetailphonePage {
   index : any;
   phones : Array<any>;
 
-  constructor(private navCtrl: NavController, navParams: NavParams ,private alertController: AlertController,public menu: MenuController) {
+  constructor(private navCtrl: NavController, navParams: NavParams ,private alertController: AlertController) {
     this.index = navParams.get('index');
     this.phones = navParams.get('phones');
     this.selectedItem = this.phones[this.index];
   }
 
-// private swipeEvent($event): void {
-//     if(this.index<this.phones.length-1 && $event.direction ==2){     
-//       this.index = this.index+1;    
-//       this.selectedItem  = this.phones[this.index];   
-//     }
-//     else if (this.index>0 && $event.direction ==4){
-//             this.index = this.index-1;    
-//             this.selectedItem  = this.phones[this.index];   
-//     }
-//   }
-  ionViewDidEnter() {
-    // the root left menu should be disabled on the tutorial page
-    this.menu.enable(false);
+private swipeEvent($event): void {
+    if(this.index<this.phones.length-1 && $event.direction ==2){
+      console.log('Swip gauche');     
+      this.index = this.index+1;    
+      this.selectedItem  = this.phones[this.index];   
+    }
+    else if (this.index>0 && $event.direction ==4){
+            console.log('Swip droit');     
+            this.index = this.index-1;    
+            this.selectedItem  = this.phones[this.index];   
+    }
   }
-
-  ionViewWillLeave() {
-    // enable the root left menu when leaving the tutorial page
-    this.menu.enable(true);
-  }
-
 }
-
-  // console.log('Swipe',$event.direction);
-  // console.log('index', this.index);
